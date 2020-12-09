da1a3d9e1d550f542773faf523317141e906df93
cheneyhu <cheney.hu@grapecity.com>
2020-12-09 15:31:47 +0900

    20201209002

--- screens/UserLogin.js
+++ screens/UserLogin.js
@@ -78,7 +78,7 @@ class Login extends React.Component {
         <View style={styles.footerContainer}>
           <View style={styles.footer}>
             <Text style={styles.footerTitle}>
-              {"Version: 1.0.9"}
+              {"Version: 1.0.10"}
             </Text>
           </View>
         </View>
